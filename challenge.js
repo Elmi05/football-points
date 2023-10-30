5c6334c7319963651bb2cbf55fa732d3ac45dfb7
Elmi05 <karshe0548@gmail.com>
2023-10-30 15:31:59 +0300

    finished assignment

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(wins, draws, losses) {
+    return (wins * 3) + (draws * 1) + (losses * 0);
+  }
+  
+  let teamPoints = calculatePoints(5, 2, 3);
+  console.log(teamPoints);
+  
